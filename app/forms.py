b034c60605c38232020f2f43c31f758fd59d7313
Wes Barnett <wes@barnett.science>
2018-07-24 19:22:07 -0400

    simplify

--- app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SubmitField, RadioField, FieldList, SelectField
-from wtforms.validators import DataRequired
-
-class SubmissionForm(FlaskForm):
-    select = SelectField(choices=[('10','10'),('100','100'),('1000','1000')], validators = [DataRequired()], default='100')
-    radio = FieldList(RadioField(choices=[('NA','NA'),('1','1'),('2','2'),('3','3'),('4','4'),('5','5')],
-        default='NA', validators=[DataRequired()]), min_entries=100)
-    submit = SubmitField('Submit')
