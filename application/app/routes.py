8f66c8f1eb8a7ea3f9648cd069e2dd7873ba3152
Wes Barnett <wes@barnett.science>
2018-08-03 07:58:14 -0400

    rm unneeded imports

--- application/app/routes.py
+++ application/app/routes.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from flask import render_template, flash, redirect, url_for, session
+from flask import render_template, redirect, session
 from app import app
 from app.forms import SubmissionForm
 from joblib import load, dump
